f2c57e617edb88085561f6ee833a9fed246ed96f
Kristaps <53013669+siltais@users.noreply.github.com>
2024-03-08 16:56:40 +0200

    4.4 - 4.7

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -1,7 +1,78 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
     return 1
   }
   
-  module.exports = {
-    dummy
+
+const totalLikes = (blogs) => {
+  const initialValue = 0
+  const sumWithInitial = blogs.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.likes,
+    initialValue,
+  )
+  return sumWithInitial
+}
+
+const favoriteBlog = (blogs) => {
+
+  let maxLikes = 0
+  let hasMaxLikes = 0
+
+  blogs.map((blog, i) => {
+    if(blog.likes > maxLikes){
+      maxLikes = blog.likes
+      hasMaxLikes = i
+    }
+  })
+
+  const favorite = {
+    title: blogs[hasMaxLikes].title,
+    author: blogs[hasMaxLikes].author,
+    likes: blogs[hasMaxLikes].likes
   }
+
+  return favorite
+}
+
+const mostBlogs = (blogs) => {
+
+  const groupedBlogs = _.groupBy(blogs, 'author')
+  const maxCount = _.max(_.map(groupedBlogs, group => group.length))
+  const objectWithMaxCount = _.find(groupedBlogs, group => group.length === maxCount)
+  
+  const maxBlogs = {
+    author: objectWithMaxCount[0].author,
+    blogs: objectWithMaxCount.length
+  }
+    
+  return maxBlogs
+}
+
+const mostLikes = (blogs) => {
+  
+  const groupedBlogs = _.groupBy(blogs, 'author')
+  const likesCount = {}
+  
+  _.forEach(groupedBlogs, (personLikes, author) => {
+    likesCount[author] = _.sumBy(personLikes, 'likes')
+  })
+  
+  const maxLikesPerson = _.maxBy(Object.keys(likesCount), person => likesCount[person])
+  
+  const maxLikes = {
+    author: maxLikesPerson,
+    likes: likesCount[maxLikesPerson]
+  }
+    
+  return maxLikes
+
+}
+
+module.exports = {
+  dummy, 
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
+}
