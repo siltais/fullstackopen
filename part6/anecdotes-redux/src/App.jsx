3b2d2e5be7b56037954e1d4e58134662fbb6513f
Kristaps <53013669+siltais@users.noreply.github.com>
2024-03-25 18:42:32 +0200

    6.7

--- part6/anecdotes-redux/src/App.jsx
+++ part6/anecdotes-redux/src/App.jsx
@@ -1,4 +1,5 @@
-import { voteAnecdote, createAnecdote } from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
+import { voteAnecdote } from './reducers/anecdoteReducer'
 import { useSelector, useDispatch } from 'react-redux'
 
 
@@ -12,12 +13,7 @@ const App = () => {
     dispatch(voteAnecdote(id))
   }
 
-  const createNew = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
-  }
+
 
   return (
     <div>
@@ -33,11 +29,7 @@ const App = () => {
           </div>
         </div>
       )}
-      <h2>create new</h2>
-      <form onSubmit={createNew}>
-        <div><input name = "anecdote" /></div>
-        <button type = "submit">create</button>
-      </form>
+    <AnecdoteForm />
     </div>
   )
 }
