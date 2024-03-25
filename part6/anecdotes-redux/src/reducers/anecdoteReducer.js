0241e01fc145ab4d0313ee9b20a0e4e468ac215a
Kristaps <53013669+siltais@users.noreply.github.com>
2024-03-25 17:58:48 +0200

    6.5

--- part6/anecdotes-redux/src/reducers/anecdoteReducer.js
+++ part6/anecdotes-redux/src/reducers/anecdoteReducer.js
@@ -32,7 +32,7 @@ const reducer = (state = initialState, action) => {
       }
       return state.map(anecdote => 
         anecdote.id !== id ? anecdote : votedAnecdote
-      )
+      ).sort((a, b) => b.votes - a.votes)
     }
     default:
       return state
