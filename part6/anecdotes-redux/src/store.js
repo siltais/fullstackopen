c18dbc9efd1474bb801787020eeedd8226261d64
Kristaps <53013669+siltais@users.noreply.github.com>
2024-03-27 11:02:15 +0200

    6.16 & 6.17

--- part6/anecdotes-redux/src/store.js
+++ part6/anecdotes-redux/src/store.js
@@ -1,9 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
-import anecdoteReducer, { setAnecdotes } from './reducers/anecdoteReducer'
+import anecdoteReducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
 import notificationReducer from './reducers/notificationReducer'
-
-import anecdoteService from './services/anecdotes'
 
 const store = configureStore({
   reducer: {
@@ -13,8 +11,6 @@ const store = configureStore({
   }
 })
 
-anecdoteService.getAll().then(anecdotes =>
-  store.dispatch(setAnecdotes(anecdotes))
-)
+
 
 export default store
