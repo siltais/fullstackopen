cdb8b27665896f686f7c6b59efd141a68b59174a
Kristaps <53013669+siltais@users.noreply.github.com>
2024-04-10 12:09:12 +0300

    8.7

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -135,6 +135,10 @@ const typeDefs =`
       author: String!
       genres: [String!]!
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
   `
 
@@ -193,6 +197,15 @@ const resolvers = {
         authors = authors.concat(newAuthor)
       }
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+      if (!author) {
+        return null
+      }
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(a=> a.name === args.name ? updatedAuthor : a)
+      return updatedAuthor
     }
   }
 }
